3ff4983e42d7d3b6c698f55fc660bf4fcd1e479b
PatrickGrilanda <patrick.grilanda26@gmail.com>
2024-03-08 15:54:56 -0300

    Added PHPUnit

--- index.php
+++ index.php
@@ -2,20 +2,26 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-//cria um item
+//cria um itens
 $item = new App\Item('Cadeira', 200);
+$item2 = new App\Item('Mesa', 300);
 
 //cria um carrinho de compras
 $cart = new App\CarrinhoCompra();
 
 //adiciona o item ao carrinho
 $cart->getItens()->addItem($item);
+$cart->getItens()->addItem($item2);
 
 //cria um pedido
 $order = new App\Pedido($cart);
 
 //remove o item do carrinho
 $order->getCart()->removeItem($item);
+
+echo '<pre>';
+print_r($order);
+echo'</pre>';
 
 //se o pedido foi confirmado, envia um email
 if($order->confirmOrder()) {
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,6 +31,18 @@ class ComposerAutoloaderInit4d3c7415e9616fb490d1c71ae43d8707
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit4d3c7415e9616fb490d1c71ae43d8707::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
